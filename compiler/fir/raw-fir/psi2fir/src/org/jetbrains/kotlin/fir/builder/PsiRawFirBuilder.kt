437d01375d53826eabf8fe01b9debc44672fb9bd
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-03-07 13:33:33 +0200

    [FIR] Make `FirWhenSubjectExpression` into a property access expression
    Deleting `FirWhenSubjectExpression` altogether seems inconvenient:
    we may still need to tell apart `$subj$ is Type` from `subject is Type`,
    and having a separate node is quite handy. What we really want is to
    simplify the DFA logic related to `when` subjects, which we can do by
    offloading it to regular property accesses.
    
    The change in `coneDiagnosticToFirDiagnostic.kt` is needed to prevent
    "strange" errors on short conditions like `is Type ->` or `value ->`,
    like `<!UNRESOLVED_REFERENCE!>""<!>`. See `guardWithNoSubject.kt`.
    
    The change in `VariableInitializationCheckProcessor` is required to
    prevent `<!UNINITIALIZED_VARIABLE!>is Type<!> ->`. See
    `unsupportedVariableDeclarationsInWhenSubject.kt`.
    
    ^KT-75704 Fixed

--- compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/PsiRawFirBuilder.kt
+++ compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/PsiRawFirBuilder.kt
@@ -2941,7 +2941,7 @@ open class PsiRawFirBuilder(
                             buildWhenBranch(hasGuard = entryGuard != null) {
                                 source = entrySource
                                 condition = entry.conditions.toFirWhenCondition(
-                                    ref,
+                                    subjectVariable,
                                     { errorReason, fallbackSource ->
                                         toFirExpression(errorReason, sourceWhenInvalidExpression = fallbackSource)
                                     },
@@ -2965,20 +2965,22 @@ open class PsiRawFirBuilder(
                                                 )
                                             } else {
                                                 shouldBind = true
-                                                buildErrorExpression {
-                                                    source = condition.toFirSourceElement()
-                                                    nonExpressionElement = condition.toFirWhenCondition(
-                                                        ref,
-                                                        { errorReason, fallbackSource ->
+                                                val convertedCondition = condition.toFirWhenCondition(
+                                                    subjectVariable,
+                                                    { errorReason, fallbackSource ->
                                                             toFirExpression(errorReason, sourceWhenInvalidExpression = fallbackSource)
                                                         },
-                                                        { toFirOrErrorType() },
-                                                    )
-                                                    diagnostic = ConeSimpleDiagnostic(
-                                                        "No expression in condition with expression",
-                                                        DiagnosticKind.ExpressionExpected,
-                                                    )
-                                                }
+                                                    { toFirOrErrorType() },
+                                                )
+                                                convertedCondition.takeIf { subjectVariable != null }
+                                                    ?: buildErrorExpression {
+                                                        source = condition.toFirSourceElement()
+                                                        this.nonExpressionElement = convertedCondition
+                                                        diagnostic = ConeSimpleDiagnostic(
+                                                            "No expression in condition with expression",
+                                                            DiagnosticKind.ExpressionExpected,
+                                                        )
+                                                    }
                                             }
                                         })
                                     }.guardedBy(entryGuard)
